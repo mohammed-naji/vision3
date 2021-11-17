f6759a2f4c5b897ce4176a3776fddf712b19de7d
Mohammed <m.naji1512@gmail.com>
2021-11-17 18:39:23 +0200

    lec5

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return 'Homepage';
 });
 
-Route::get('new', function() {
-    return 'New Route';
+Route::get('about', function() {
+    return 'About Page';
 });
+
+Route::get('team', function() {
+    return 'Team Page';
+});
+
+Route::get('Services', function() {
+    return 'Services Page';
+});
+
+Route::get('contact', function() {
+    return 'contact Page';
+});
+
+Route::get('user', function() {
+    return 'User Page';
+});
+
+Route::get('user/profile/main', function() {
+    return 'profile Page';
+});
+
+Route::get('student/{name?}/{age?}', function($name = '', $age = '') {
+    return 'Welcome ' . $name . ' ' . $age;
+});
+
+// Routes Type
+// Route::get('route_url', 'Action');
+// Route::post('route_url', 'Action');
+// Route::put('route_url', 'Action');
+// Route::patch('route_url', 'Action');
+// Route::delete('route_url', 'Action');
